91b4df4551b66671b41e619d46da8f98e0d914e1
Carla Archuleta <archulca@rose-hulman.edu>
2019-01-27 18:37:44 -0500

    TODO #2

--- src/m8_loops_and_objects.py
+++ src/m8_loops_and_objects.py
@@ -11,8 +11,9 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
 
 import rosegraphics as rg
 
-
+import math as m
 def main():
+    print_sequence1()
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
 
@@ -29,7 +30,7 @@ def print_sequence1():
        200
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this function, per its doc-string above.
+    # DONE: 2. Implement this function, per its doc-string above.
     # Put a statement in  main  to test this function.
     # REQUIREMENT: You must use a   RANGE  statement to solve this problem.
     # -------------------------------------------------------------------------
@@ -37,7 +38,8 @@ def print_sequence1():
     print('--------------------------------------------------')
     print('Running print_sequence1:')
     print('--------------------------------------------------')
-
+    for k in range(21):
+        print(10*k)
 
 def draw_circles1():
     """
